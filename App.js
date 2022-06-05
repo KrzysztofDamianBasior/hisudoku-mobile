41d11b438282264ad8a1d18401e192eb1b74178a
Krzysztof Damian Basior <KrzysztofDamianBasior@gmail.com>
2022-06-05 18:44:40 +0200

    feat: Add custom fonts

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
-import * as React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import 'react-native-gesture-handler' //required by stack navigation
 import { StatusBar } from 'expo-status-bar'
 import { NavigationContainer } from '@react-navigation/native'
 import TabNavigator from './navigation/TabNavigator'
+import * as Font from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+import { View } from 'react-native'
+
+let customFonts = {
+  Alegreya: require('./assets/fonts/Alegreya-VariableFont_wght.ttf'),
+  'Alegreya-Italic': require('./assets/fonts/Alegreya-Italic-VariableFont_wght.ttf'),
+}
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false)
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Keep the splash screen visible while we fetch resources
+        await SplashScreen.preventAutoHideAsync()
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync(customFonts)
+      } catch (e) {
+        console.warn(e)
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true)
+      }
+    }
+    prepare()
+  }, [])
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync()
+    }
+  }, [appIsReady])
+
+  if (!appIsReady) {
+    return null
+  }
+
   return (
-    <NavigationContainer>
+    <NavigationContainer onLayout={onLayoutRootView}>
       <TabNavigator />
       <StatusBar style="auto" />
     </NavigationContainer>
--- navigation/SettingsStack.js
+++ navigation/SettingsStack.js
@@ -1,12 +1,24 @@
 /* eslint-disable react/display-name */
 
 import * as React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 export default () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Settings Screen</Text>
+      <Text style={styles.text}>Settings Screen</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontFamily: 'Alegreya',
+  },
+})
