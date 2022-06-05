e416f84dba33fa3b76af52f66e39677f48e01ce6
Krzysztof Damian Basior <KrzysztofDamianBasior@gmail.com>
2022-06-05 20:52:03 +0200

    fix: Modify the way fonts are loaded

--- App.js
+++ App.js
@@ -3,9 +3,8 @@ import 'react-native-gesture-handler' //required by stack navigation
 import { StatusBar } from 'expo-status-bar'
 import { NavigationContainer } from '@react-navigation/native'
 import TabNavigator from './navigation/TabNavigator'
-import * as Font from 'expo-font'
-import * as SplashScreen from 'expo-splash-screen'
-import { View } from 'react-native'
+import { useFonts } from 'expo-font'
+import { View, Text } from 'react-native'
 
 let customFonts = {
   Alegreya: require('./assets/fonts/Alegreya-VariableFont_wght.ttf'),
@@ -13,41 +12,18 @@ let customFonts = {
 }
 
 export default function App() {
-  const [appIsReady, setAppIsReady] = useState(false)
+  let [fontsLoaded] = useFonts(customFonts)
 
-  useEffect(() => {
-    async function prepare() {
-      try {
-        // Keep the splash screen visible while we fetch resources
-        await SplashScreen.preventAutoHideAsync()
-        // Pre-load fonts, make any API calls you need to do here
-        await Font.loadAsync(customFonts)
-      } catch (e) {
-        console.warn(e)
-      } finally {
-        // Tell the application to render
-        setAppIsReady(true)
-      }
-    }
-    prepare()
-  }, [])
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      // This tells the splash screen to hide immediately! If we call this after
-      // `setAppIsReady`, then we may see a blank screen while the app is
-      // loading its initial state and rendering its first pixels. So instead,
-      // we hide the splash screen once we know the root view has already
-      // performed layout.
-      await SplashScreen.hideAsync()
-    }
-  }, [appIsReady])
-
-  if (!appIsReady) {
-    return null
+  if (!fontsLoaded) {
+    return (
+      <View>
+        <Text>Loading</Text>
+      </View>
+    )
   }
 
   return (
-    <NavigationContainer onLayout={onLayoutRootView}>
+    <NavigationContainer>
       <TabNavigator />
       <StatusBar style="auto" />
     </NavigationContainer>
